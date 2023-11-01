3e5fbc5e1a22d25d8df2da6c299817b2c136fc61
chtw2001 <jungtaekwon1019@gmail.com>
2023-11-01 19:07:33 +0900

    create models

--- vote_rate/admin.py
+++ vote_rate/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
-#from vote_rate.models import rate
+from vote_rate.models import candidate
+
+# class CandidateAdmin(admin.ModelAdmin):
+#     readonly_fields = ('name', 'total_student_num', 'category', 'time')
+
+admin.site.register(candidate)
+
+# class CandidateAdmin(admin.ModelAdmin):
+#     readonly_fields = ('name', 'total_student_num', 'category', 'time')
+
+# admin.site.register(candidate, CandidateAdmin)
--- vote_rate/models.py
+++ vote_rate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class candidate(models.Model):
+    name = models.CharField(max_length=128, null=False, primary_key=True)
+    total_student_num = models.IntegerField(null=False)
+    voted_student_num = models.IntegerField(null=False)
+    win = models.CharField(max_length=128, null=True)
+    category = models.CharField(max_length=128, null=True)
+    time = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+        
