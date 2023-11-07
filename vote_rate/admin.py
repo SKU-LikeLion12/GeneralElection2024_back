4eae76affe5958d1a43beba4334e0000fbf8fa66
chtw2001 <jungtaekwon1019@gmail.com>
2023-11-07 22:23:57 +0900

    make database

--- vote_rate/admin.py
+++ vote_rate/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
 from vote_rate.models import Candidate, Refresh
 
-# class CandidateAdmin(admin.ModelAdmin):
-#     readonly_fields = ['name']
+class CandidateAdmin(admin.ModelAdmin):
+    readonly_fields = ['name']
 
-#     def has_add_permission(self, request):
-#         return False
+    def has_add_permission(self, request):
+        return False
     
-#     def has_delete_permission(self, request, obj=None):
-#         return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
-# admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Candidate, CandidateAdmin)
 
 
-# class RefreshAdmin(admin.ModelAdmin):
-#     def has_add_permission(self, request):
-#         return False
+class RefreshAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
     
-#     def has_delete_permission(self, request, obj=None):
-#         return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
-# admin.site.register(Refresh, RefreshAdmin)
+admin.site.register(Refresh, RefreshAdmin)
 
-admin.site.register(Refresh)
-admin.site.register(Candidate)
+# admin.site.register(Refresh)
+# admin.site.register(Candidate)
